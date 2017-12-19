cdafeebf0c61e930d7f7f0cca88746cc297bccf0
Yury Takzhanov <takzhanov@restream.rt.ru>
2017-12-19 21:50:53 +0300

    hw09 start layout

---
+++ hw09-jdbc/src/main/java/io/github/takzhanov/umbrella/hw09/Main.java
@@ -0,0 +1,4 @@
+package io.github.takzhanov.umbrella.hw09;
+
+public class Main {
+}
---
+++ hw09-jdbc/src/test/java/io/github/takzhanov/umbrella/hw09/MainTest.java
@@ -0,0 +1,7 @@
+package io.github.takzhanov.umbrella.hw09;
+
+import static org.junit.Assert.*;
+
+public class MainTest {
+
+}
